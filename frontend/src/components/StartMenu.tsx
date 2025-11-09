0fc436789581d041cfce292a22e4dbb06ba4b1d9
June Delos Reyes <julia.delosreyes15@gmail.com>
2025-11-09 21:30:30 +1100

    Added basic components to start menu

--- frontend/src/components/StartMenu.tsx
+++ frontend/src/components/StartMenu.tsx
@@ -1,11 +1,25 @@
-const StartMenu = () => {  
+import { Link } from "react-router-dom";
+import { Button, Modal } from "@mui/material";
+import { useState, useEffect, useRef } from "react";
+
+const StartMenu = () => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   return (
     <>
-      <main className="flex justify-center items-center w-screen h-screen text-[#FFFFFF]">
-        <span className="text-3xl">Start Menu</span>
+      <main className="flex flex-col justify-center items-center w-screen h-screen gap-3 text-[#FFFFFF]">
+        <span className="text-4xl">Not Osu!</span>
+        <Button variant="contained">
+          <Link to="/select">Start!</Link>
+        </Button>
+        <Button onClick={handleOpen}>Settings</Button>
+        <Modal onClose={handleClose} open={open}>
+          <h2 className="text-[#FFFFFF]">hello settings here</h2>
+        </Modal>
       </main>
     </>
-  )
-}
+  );
+};
 
-export default StartMenu
+export default StartMenu;
