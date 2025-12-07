d9b2a8886a7e129930462a351a96266669398621
June Delos Reyes <julia.delosreyes15@gmail.com>
2025-12-07 21:47:21 +1100

    Settings modal slight styling

--- frontend/src/components/Settings.tsx
+++ frontend/src/components/Settings.tsx
@@ -10,12 +10,13 @@ const Settings = ({ open, onClose }: SettingsProps) => {
   return (
     <Modal open={open} onClose={onClose}>
       <div className="flex flex-col justify-center items-center w-screen h-screen gap-3 text-[#FFFFFF]">
-        <div>Hello awesome Settings here</div>
-        <div>
-          <Button onClick={onClose} variant="contained">
-            {" "}
-            close awesome settings
-          </Button>
+        <div className=" bg-purple-950 rounded-4xl p-8">
+          <div>Hello awesome Settings here</div>
+          <div>
+            <Button onClick={onClose} variant="contained">
+              close awesome settings
+            </Button>
+          </div>
         </div>
       </div>
     </Modal>
