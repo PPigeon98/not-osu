f7b3e94612c547cc27199a00db80ef0f88c40628
Brian Wifanie <brianwifanie@gmail.com>
2025-12-21 01:54:19 +1100

    Added letter grades to scores

--- frontend/src/components/PassedMap.tsx
+++ frontend/src/components/PassedMap.tsx
@@ -15,12 +15,50 @@ const PassedMap = () => {
     scores: LeaderboardScore[];
   }) || {};
 
+  const getRank = (accuracy: number) => {
+    let rank: string;
+    let rankColor: string;
+
+    switch (true) {
+      case accuracy === 100:
+        rank = "SS";
+        rankColor = "#e9be8cff";
+        break;
+      case accuracy >= 95:
+        rank = "S";
+        rankColor = "#e9e48cff";
+        break;
+      case accuracy >= 90:
+        rank = "A";
+        rankColor = "#9ae98cff";
+        break;
+      case accuracy >= 80:
+        rank = "B";
+        rankColor = "#8cb1e9ff";
+        break;
+      case accuracy >= 70:
+        rank = "C";
+        rankColor = "#d58ce9ff";
+        break;
+      default:
+        rank = "D";
+        rankColor = "#e98c8cff";
+    }
+
+    return { rank, rankColor };
+  }
+
+  const { rank, rankColor } = getRank(accuracy);
+
   return (
     <main className="flex flex-col items-center w-screen h-screen text-[#FFFFFF]">
       <div className="text-[8vw] font-bold mt-10">You Passed!</div>
 
       <div className="flex w-full h-full justify-between">
         <div className="flex flex-col w-full gap-4 justify-center items-center mb-8">
+          <span className="text-[5vw] font-bold" style={{ color: rankColor }}>
+            {rank}
+          </span>
           <div className="text-[2vw] font-bold">
             <span>Final Score: </span>
             <span>{score.toLocaleString()}</span>
@@ -40,23 +78,31 @@ const PassedMap = () => {
           {[...scores]
             .sort((a, b) => b.score - a.score)
             .slice(0, 5)
-            .map((leaderboardScore, i) => (
-              <div
-                key={i}
-                className={`flex w-[50%] p-2 gap-5 bg-opacity-50 ${
-                  leaderboardScore.score === score &&
-                  leaderboardScore.highestCombo === highestCombo &&
-                  Math.abs(leaderboardScore.accuracy - accuracy) < 0.001
-                    ? "bg-yellow-500"
-                    : ""
-                }`}
-              >
-                <span>{i + 1}</span>
-                <span className="opacity-60">
-                  {leaderboardScore.score.toLocaleString()}
-                </span>
-              </div>
-            ))}
+            .map((leaderboardScore, i) => {
+              const { rank, rankColor } = getRank(leaderboardScore.accuracy);
+
+              return (
+                <div
+                  key={i}
+                  className={`flex w-[50%] p-2 bg-opacity-50 justify-between ${
+                    leaderboardScore.score === score &&
+                    leaderboardScore.highestCombo === highestCombo &&
+                    Math.abs(leaderboardScore.accuracy - accuracy) < 0.001
+                      ? "bg-yellow-500"
+                      : ""
+                  }`}
+                >
+                  <div className="flex gap-5">
+                    <span>{i + 1}</span>
+                    <span className="opacity-60">
+                      {leaderboardScore.score.toLocaleString()}
+                    </span>
+                  </div>
+                  
+                  <span style={{ color: rankColor }}>{rank}</span>
+                </div>
+              );
+            })}
         </div>
       </div>
 
